bad5ad0955a49368f83f45dcc0192874827f9b9a
Abdirahman Mohamed <Abdimo1324@gmail.com>
2024-08-26 23:36:47 -0500

    Did some changes

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,5 +1,35 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
+let myCollection = [];
+function addToCollection(Collection, title, artist, yearPublished){
+   const newObject = {
+      title: title,
+      artist: artist,
+      yearPublished: yearPublished,
+   };
+   Collection.push[newObject];
+
+   return newObject;
+};
+
+
+console.log(myCollection.push("Empire state of mind", "Jay-z", 2009));
+console.log(myCollection.push("lollipop", "lil wayne", 2008));
+console.log(myCollection.push("Mirror", "Lil wayne", 2011));
+console.log(myCollection.push("Family matters", "Drake", 2024));
+console.log(myCollection.push("one Dance", "Drake", 2016));
+console.log(myCollection.push("Passionfruit", "Drake", 2017));
+
+console.log(myCollection);
+
+function showCollection(collection){
+  for (let i = 0; i < myCollection.length; i++) {
+    console.log(`$`)
+    
+  }
+}
+
+
 
 
 
